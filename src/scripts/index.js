2dca2b3f84d9cfacfd6470708f12079bdf865203
Alex <alevaliev@yandex.ru>
2021-02-02 15:31:32 +0300

    changed index page and head partial templates

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,25 @@
 import '../styles/index.scss';
+import $ from "jquery";
+import LazyLoad from 'vanilla-lazyload';
+import WOW from 'wow.js';
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.pug');
 }
 
-console.log('webpack starterkit');
+$(function() {
+  var lazyLoadInstance = new LazyLoad({
+    elements_selector: '.lazy',
+  });
+
+  var wow = new WOW({
+    boxClass:     'wow',      
+    animateClass: 'animate__animated',     
+    mobile:       true,       
+    live:         true,       
+    scrollContainer: null,    
+    resetAnimation: true, 
+  });
+
+  wow.init();
+});
